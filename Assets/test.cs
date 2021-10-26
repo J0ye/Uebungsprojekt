704e3f0155e3d2b69191e589714b91c73fefda50
Jonathan Müller <ahmad.abo.louha@fh-erfurt.de>
2021-10-26 13:00:34 +0200

    New script

--- Assets/test.cs
+++ Assets/test.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 1, 0) * speed);
     }
 }
